15aae1e6d57538c40b884a4d20129cd1ee073fa2
shinjunsuuu <sjs438@gmail.com>
2025-11-14 12:37:52 +0900

    [feat] #4 - 로그인 DTO 구현

--- src/main/java/com/cloudrangers/cloudpilot/dto/request/LoginRequest.java
+++ src/main/java/com/cloudrangers/cloudpilot/dto/request/LoginRequest.java
@@ -1,4 +1,17 @@
 package com.cloudrangers.cloudpilot.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginRequest {
+    @NotNull(message = "사번(empno)은 필수 입력값입니다.")
+    private Long empno;
+
+    @NotNull(message = "비밀번호는 필수 입력값입니다.")
+    @Size(min = 4, max = 100, message = "비밀번호는 8자 이상 100자 이하로 입력해주세요.")
+    private String password;
 }
--- src/main/java/com/cloudrangers/cloudpilot/dto/response/LoginResponse.java
+++ src/main/java/com/cloudrangers/cloudpilot/dto/response/LoginResponse.java
@@ -1,4 +1,16 @@
 package com.cloudrangers.cloudpilot.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginResponse {
+
+    private String accessToken;
+    private String role;
+    private String team;
+    private String username;
 }
