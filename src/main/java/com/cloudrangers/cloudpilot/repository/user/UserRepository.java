653182fb0a9e151c228618bfc49188f1fe5a2172
shinjunsuuu <sjs438@gmail.com>
2025-11-14 12:38:00 +0900

    [feat] #4 - UserRepository 구현

--- src/main/java/com/cloudrangers/cloudpilot/repository/user/UserRepository.java
+++ src/main/java/com/cloudrangers/cloudpilot/repository/user/UserRepository.java
@@ -1,4 +1,16 @@
 package com.cloudrangers.cloudpilot.repository.user;
 
-public class UserRepository {
+import com.cloudrangers.cloudpilot.domain.user.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmpno(Long empno);
+    Optional<User> findByEmail(String email);
+
+    // ✅ 로그인 시 user → userRoles → role, team 까지 즉시 로딩
+    @EntityGraph(attributePaths = {"userRoles", "userRoles.role", "userRoles.team"})
+    Optional<User> findWithRolesByEmpno(Long empno);
 }
