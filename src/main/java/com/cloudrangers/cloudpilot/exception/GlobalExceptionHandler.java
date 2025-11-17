9b01a51e2172d27c693bed28d24365fccb850100
shinjunsuuu <sjs438@gmail.com>
2025-11-17 10:17:08 +0900

    [refactor] #4 - 예외 처리 구조 개선 및 오류 메시지 정리

--- src/main/java/com/cloudrangers/cloudpilot/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cloudrangers/cloudpilot/exception/GlobalExceptionHandler.java
@@ -1,4 +1,27 @@
 package com.cloudrangers.cloudpilot.exception;
 
+import com.cloudrangers.cloudpilot.common.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CovigatorException.class)
+    public ApiResponse<Object> handleCovigatorException(CovigatorException ex) {
+
+        log.error("❌ Custom Exception 발생: {}", ex.getMessage());
+
+        return ApiResponse.fail(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ApiResponse<Object> handleGeneralException(Exception ex) {
+
+        log.error("🔥 Unexpected System Error: ", ex);
+
+        return ApiResponse.fail("서버 내부 오류가 발생했습니다.");
+    }
 }
--- src/main/java/com/cloudrangers/cloudpilot/exception/badrequest/InvalidRequestException.java
+++ src/main/java/com/cloudrangers/cloudpilot/exception/badrequest/InvalidRequestException.java
@@ -1,4 +1,11 @@
 package com.cloudrangers.cloudpilot.exception.badrequest;
 
-public class InvalidRequestException {
+import com.cloudrangers.cloudpilot.exception.CovigatorException;
+import org.springframework.http.HttpStatus;
+
+public class InvalidRequestException extends CovigatorException {
+
+    public InvalidRequestException(String message) {
+        super(HttpStatus.BAD_REQUEST, 10006, message);
+    }
 }
